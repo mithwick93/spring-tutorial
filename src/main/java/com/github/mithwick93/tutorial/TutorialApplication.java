8658de9b7e4503c8d4db77b713b6a8d86cd662fa
Mithila Wickramarathne <mithwick93@gmail.com>
2021-05-11 00:28:38 +0530

    add Scheduled task
    Signed-off-by: Mithila Wickramarathne <mithwick93@gmail.com>

--- src/main/java/com/github/mithwick93/tutorial/TutorialApplication.java
+++ src/main/java/com/github/mithwick93/tutorial/TutorialApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class TutorialApplication {
 
     public static void main(String[] args) {
@@ -17,5 +19,4 @@ public class TutorialApplication {
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
-
 }
