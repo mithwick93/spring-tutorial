04484ebc9ab6e931c40ac834a6da1ad7ac56f565
Mithila Wickramarathne <mithwick93@gmail.com>
2021-05-11 00:27:04 +0530

    use Response entity
    Signed-off-by: Mithila Wickramarathne <mithwick93@gmail.com>

--- src/main/java/com/github/mithwick93/tutorial/gateway/QuotersGateway.java
+++ src/main/java/com/github/mithwick93/tutorial/gateway/QuotersGateway.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,13 +25,19 @@ public class QuotersGateway {
 
     public Quote getSpringBootQuote() {
         try {
-            Quote quote = restTemplate.getForObject(
+//            Quote quote = restTemplate.getForObject(
+//                    QUOTERS_URL.concat("/random"),
+//                    Quote.class
+//            );
+
+            ResponseEntity<Quote> quoteResponseEntity = restTemplate.getForEntity(
                     QUOTERS_URL.concat("/random"),
                     Quote.class
             );
 
-            assert quote != null;
-            LOG.info("Got the quote: " + quote);
+            LOG.info("Got the quote response: " + quoteResponseEntity);
+            Quote quote = quoteResponseEntity.getBody();
+            LOG.info("Got the quote: " + quoteResponseEntity.getBody());
             return quote;
         } catch (Exception ex) {
             LOG.error("Exception while getting quote", ex);
